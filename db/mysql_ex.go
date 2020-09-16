2be2c02a5d0ee64adbd8444e9b8da9dbb97636a4
JellyZhang <zhangjelly0405@gmail.com>
2020-09-16 23:16:12 +0800

    feat: mysql connect

---
+++ db/mysql_ex.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+const (
+	timeout = "10s" //连接超时，10秒
+)
+
+func ConnectMysql(host string, port int32, username, password, dbName string) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=True&loc=Local&timeout=%s",
+		username,
+		password,
+		host,
+		port,
+		dbName,
+		timeout)
+
+	log.Printf("[jnvwa] connect mysql with host=%s, port=%d, username=%s, password=%s, dbName=%s", host, port, username, password, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ db/mysql_ex_test.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnectMysql(t *testing.T) {
+	type args struct {
+		host     string
+		port     int32
+		username string
+		password string
+		dbName   string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "连接成功",
+			args: args{
+				host:     "10.129.50.89",
+				port:     3306,
+				username: "root",
+				password: "123456",
+				dbName:   "mybase",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := ConnectMysql(tt.args.host, tt.args.port, tt.args.username, tt.args.password, tt.args.dbName)
+			assert.Equal(t, err, nil)
+			assert.NotEqual(t, res, nil)
+		})
+	}
+}
